3f706b7c9c4ecbf5055223f53486ed55b3630975
RefiyeK <205158404+RefiyeK@users.noreply.github.com>
2025-10-07 16:26:03 +0200

    aktuell

--- script.js
+++ script.js
@@ -1,84 +1,178 @@
-document.addEventListener('DOMContentLoaded', () => {
-   const images = [
-    {src: "img/art-hippie-van-flowers.jpg", alt: "Art Hippie Van Flowers", caption: "Art Hippie Van Flowers"},
-    {src: "img/nature-multnomah-waterfall.jpg", alt: "multnomah Waterfall Bridge", caption: "multnomah Waterfall Bridge"},
-    {src: "img/asia-buddha-temple.jpg", alt: "Buddha Statue Temple", caption: "Buddha Statue Temple"},
-    {src: "img/beach-grass-view-sunset.jpg", alt: "Tropical Beach View", caption: "Tropical Beach View"},
-    {src: "img/beach-maldives-palm-chairs.jpg", alt: "Beachs Palm Chairs", caption: "Beachs Palm Chairs"},
-    {src: "img/christmas-room.jpg", alt: "Cozy room decorated fro Chrismus", caption: "Cozy Christmas Room"},
-    {src: "img/beach-turkey-oludeniz.jpg", alt: "Ölüdeniz Beach Turkey", caption: "Ölüdeniz Beach Turkey"},
-    {src: "img/city-italy-cinque-terre.jpg", alt: "Cinque Terre Colorful Hiuses", caption: "Cinque Terre Colorful Hiuses"},
-    {src: "img/city-norway-trondheim-houses.jpg", alt: "Trondheim Houses Water", caption: "Trondheim Houses Water"},
-    {src: "img/city-prague-tram-autumn.jpg", alt: "City Transport Path", caption: "City Transport path"},
-    {src: "img/fantasy-open-book-dog-umbrella.jpg", alt: "Book Imagination Open", caption: "Book Imagination Open"},
-    {src: "img/frog-holiday-deckchair.jpg", alt: "Frog Relaxing Chair", caption: "Frog Relaxing Chair"},
-    {src: "img/frog-travel-suitcase.jpg", alt: "Frog Travel luggage", caption: "Frog Travel luggage"},
-    {src: "img/nature-brown-butterfly.jpg", alt: "Brown butterfly Leaf", caption: "Brown butterfly Leaf"},
-    {src: "img/funny-frog-under-leaf-umbrella.jpg", alt: "A bright green frog sitting on a tropical leaf", caption: "Frog sitting on a tropical leaf"},
-    {src: "img/nature-floating-green-leaf.jpg", alt: "Floating Green Leaf", caption: "Floating Green Leaf"},
-    {src: "img/funny-frogs-beach-party.jpg", alt: "Frogs Beach party", caption: "Frogs Beach party"},
-    {src: "img/greece-zakynthos-navagio-beach.jpg", alt: "Navagio Beach Greece", caption: "Navagio Beach Greece"},
-    {src: "img/hand-holding-old-book.jpg", alt: "Hand Holding Old Book", caption: "Hand Holding Old Book"},
-    {src: "img/history-egypt-hieroglyphs.jpg", alt: "Egyptian Hieroglyphs Wall", caption: "Egyptian Hieroglyphs Wall"},
-    {src: "img/landmark-mexico-soumaya-museum.jpg", alt: "Soumaya Museum Mexico", caption: "Soumaya Museum Mexico"},
-    {src: "img/landmark-monochrome-lighthouse.jpg", alt: "Black and White Lighthouse", caption: "Black and White Lighthouse"},
-    {src: "img/nature-alps-lake-village.jpg", alt: "Alpine Lake Village", caption: "Alpine Lake Village"},
-    {src: "img/nature-alps-waterfall-river.jpg", alt: "Waterfall nature View", caption: "Waterfall nature View"},
-    {src: "img/nature-mossy-tree-avenue.jpg", alt: "", caption: "Mossy Tree Avenue Path"},  
-    {src: "img/nature-huge-angel-oak-tree.jpg", alt: "Huge Angel Oak Tree", caption: "Huge Angel Oak Tree"},                                                                                     
-    {src: "img/nature-stone-circle-hills.jpg", alt: "Green landscape Stones", caption: "Green landscape Stones"},
-    {src: "img/orange-balloons-blue-sky.jpg", alt: "Orange Ballons Sky", caption: "Orange Ballons Sky"},
-    {src: "img/people-couple-kiss-sunset.jpg", alt: "Couple kissing Sunset", caption: "Couple kissing Sunset"},
-    {src: "img/sea-cave-beach-entrance.jpg", alt: "Sea Cave Entrance", caption: "Sea Cave Entrance"},
-    {src: "img/travel-vietnam-golden-bridge.jpg", alt: "Golden Bridge Vietnam", caption: "Golden Bridge Vietnam"},
-    {src: "img/travel-zanzibar-rock-restaurant.jpg", alt: "Rock Restaurant Zanzibar", caption: "Rock Restaurant Zanzibar"},
-    {src: "img/tropical-beach-waves-tree.jpg", alt: "Tropical Beach Waves Tree", caption: "Tropical Beach Waves Tree"},
-    {src: "img/venice-bridge-of-sighs-boat.jpg", alt: "Venice Canal Bridge", caption: "Venice Canal Bridge"},
-  ];
+'use strict'
 
-const galleryContainer = document.getElementById('gallery_container');
-let galleryHTML = ''; // Leerer String, um den gesamten HTML-Code zu speichern
-
+//array von Fotos erstellen (den Pfad angabe)
+const myImages = [
+    "img/art-hippie-van-flowers.jpg",         
+    "img/nature-multnomah-waterfall.jpg",          
+    "img/asia-buddha-temple.jpg",            
+    "img/beach-grass-view-sunset.jpg",      
+    "img/beach-maldives-palm-chairs.jpg",  
+    "img/christmas-room.jpg",               
+    "img/beach-turkey-oludeniz.jpg",          
+    "img/city-italy-cinque-terre.jpg",               
+    "img/city-norway-trondheim-houses.jpg",    
+    "img/city-prague-tram-autumn.jpg",      
+    "img/fantasy-open-book-dog-umbrella.jpg", 
+    "img/frog-holiday-deckchair.jpg",       
+    "img/frog-travel-suitcase.jpg",         
+    "img/nature-brown-butterfly.jpg",        
+    "img/funny-frog-under-leaf-umbrella.jpg",           
+    "img/nature-floating-green-leaf.jpg",   
+    "img/funny-frogs-beach-party.jpg",    
+    "img/greece-zakynthos-navagio-beach.jpg",
+    "img/hand-holding-old-book.jpg",          
+    "img/history-egypt-hieroglyphs.jpg",          
+    "img/landmark-mexico-soumaya-museum.jpg", 
+    "img/landmark-monochrome-lighthouse.jpg",      
+    "img/nature-alps-lake-village.jpg",     
+    "img/nature-alps-waterfall-river.jpg",    
+    "img/nature-mossy-tree-avenue.jpg",          
+    "img/nature-huge-angel-oak-tree.jpg",                                                                                        
+    "img/nature-stone-circle-hills.jpg",       
+    "img/orange-balloons-blue-sky.jpg",    
+    "img/people-couple-kiss-sunset.jpg",      
+    "img/sea-cave-beach-entrance.jpg",    
+    "img/travel-vietnam-golden-bridge.jpg",   
+    "img/travel-zanzibar-rock-restaurant.jpg",   
+    "img/tropical-beach-waves-tree.jpg",          
+    "img/venice-bridge-of-sighs-boat.jpg"  
+];
 
 
+// const myAltImages = [ 
+//     "Art Hippie Van Flowers",
+//     "multnomah Waterfall Bridge",
+//     "Buddha Statue Temple",
+//     "Tropical Beach View",
+//     "Beachs Palm Chairs",
+//     "Cozy room decorated fro Chrismus",
+//     "Ölüdeniz Beach Turkey",
+//     "Cinque Terre Colorful Hiuses",
+//     "Trondheim Houses Water",
+//     "City Transport Path",
+//     "Book Imagination Open",
+//     "Frog Relaxing Chair",
+//     "Frog Travel luggage",
+//     "Brown butterfly Leaf",
+//     "A bright green frog sitting on a tropical leaf",
+//     "Floating Green Leaf",
+//     "Frogs Beach party",
+//     "Navagio Beach Greece",
+//     "Hand Holding Old Book",
+//     "Egyptian Hieroglyphs Wall",
+//     "Soumaya Museum Mexico",
+//     "Black and White Lighthouse",
+//     "Alpine Lake Village",
+//     "Waterfall nature View",
+//     "Mossy Tree Avenue Path",
+//     "Huge Angel Oak Tree",
+//     "Green landscape Stones",
+//     "Orange Ballons Sky",
+//     "Couple kissing Sunset",
+//     "Sea Cave Entrance",
+//     "Golden Bridge Vietnam",
+//     "Rock Restaurant Zanzibar",
+//     "Tropical Beach Waves Tree",
+//     "Venice Canal Bridge"
+// ];
+
+
+// const myCaptionImages = [
+//     "Art Hippie Van Flowers",
+//     "multnomah Waterfall Bridge",
+//     "Buddha Statue Temple",
+//     "Tropical Beach View",
+//     "Beachs Palm Chairs",
+//     "Cozy Christmas Room",
+//     "Ölüdeniz Beach Turkey",
+//     "Cinque Terre Colorful Hiuses",
+//     "Trondheim Houses Water",
+//     "City Transport path",
+//     "Book Imagination Open",
+//     "Frog Relaxing Chair",
+//     "Frog Travel luggage",
+//     "Brown butterfly Leaf",
+//     "Frog sitting on a tropical leaf",
+//     "Floating Green Leaf",
+//     "Frogs Beach party",
+//     "Navagio Beach Greece",
+//     "Hand Holding Old Book",
+//     "Egyptian Hieroglyphs Wall",
+//     "Soumaya Museum Mexico",
+//     "Black and White Lighthouse",
+//     "Alpine Lake Village",
+//     "Waterfall nature View",
+//     "Mossy Tree Avenue Path",
+//     "Huge Angel Oak Tree",
+//     "Green landscape Stones",
+//     "Orange Ballons Sky",
+//     "Couple kissing Sunset",
+//     "Sea Cave Entrance",
+//     "Golden Bridge Vietnam",
+//     "Rock Restaurant Zanzibar",
+//     "Tropical Beach Waves Tree",
+//     "Venice Canal Bridge"
+// ];
+
+
+document.getElementById("gallery_container").innerhtml = myImages;
+
+function initArrays(){
+console.log(myImages);
+}
 
 
 
+// let startGallery= document.getElementById('gallery_container');
+// startGallery = myImages;
 
-   
-   const dialogbox = document.getElementById('dialogbox');
-const dialogboxImage = document.getElementById('dialogbox_image');
-const dialogboxCaption = document.getElementById('dialogbox_caption');
+// function startGallery (){
+//     const galleryContainer = document.getElementById('gallery_container');
+//     let galleryHTML= '';
+//     let amountGalleryImages = myImages.length;
+// }
 
+// for (let i = 0; i <= myImages.lenght; i++){
 
+// const img = myImages[i];
+// const alt = myAltImages[i];
+// const caption = myCaptionImages[i];
 
-images.forEach(image => {
-        galleryHTML += `
-            <div class="gallery_item">
-                <img src="${image.src}" alt="${image.alt}" data_caption="${image.caption}">
-                <p class="caption">${image.caption}</p>
-            </div>
-        `;
-    });
+// galleryHTML +=  `
+//         <div class="gallery_items">
+//             <img src="${img}" alt="${alt}" data-caption="${caption}">
+//             <p class="caption">${caption}</p>
+//         </div>
+//         `;
+// }
+
+// const dialogImage = document.getElementById("dialogbox_image");
+// const dialogCaption = document.getElementById("dialogbox_caption");
+// const galleryContainer = document.getElementById("gallery_container");
+
     
-    galleryContainer.innerHTML = galleryHTML; 
-
-    const galleryImages = document.querySelectorAll('.gallery_item img');
-    
-    galleryImages.forEach(image => {
-    image.addEventListener('click', () => {
-            dialogboxImage.src = image.src; 
-            dialogboxCaption.textContent = image.dataset.caption;
-            dialogbox.showModal(); //Öffnet das Dialog-Element als Modal    
-        });
-        });
 
 
-    const dialogRef = document.getElementById("myDialog")
-    function openDialog(){
+
+
+
+const dialogRef = document.getElementById("myDialog")
+    function openDialog(){                      
+        
         dialogRef.showModal();
     }
     function closeDialog(){
-        dialogRef.closest();
+        dialogRef.close();
     }
-});
+   
+
+
+    
+
+
+//Startfunktion/init erstellen im Body aufrufen
+// Funktion die Bilder rendert (für die miniatur ansicht)
+//Funktion dialog öffnen und die dialog schließt
+//Funktion für next Image
+//Funktion für vorherige Bild
+//
